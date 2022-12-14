7ddb9e5c5d32b7919114ef841cf598e38ec2fe36
Rafael Penido Rocha <72994393+rpenido19@users.noreply.github.com>
2022-12-14 17:51:29 -0300

    Título da página e ícone

--- green-paper/resources/views/layouts/app.blade.php
+++ green-paper/resources/views/layouts/app.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="icon" href="{{asset('/images/icon.png')}}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Green Paper</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
